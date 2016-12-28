e0cb0f90cc850d234516d84ea46979a3980dc93f
Hang <yhnbgfd@gmail.com>
2016-12-28 11:46:31 +0800

    按钮发鼠标事件模拟滚动

--- WebBrowser/Views/UserControls/WinFormWebBrowserUserControl.xaml.cs
+++ WebBrowser/Views/UserControls/WinFormWebBrowserUserControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using WebBrowserWPF.Base;
 
@@ -6,6 +7,20 @@ namespace WebBrowserWPF.Views.UserControls
 {
     public partial class WinFormWebBrowserUserControl : System.Windows.Controls.UserControl, IMyBrowser
     {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int flags, int dX, int dY, int buttons, int extraInfo);
+
+        const int MOUSEEVENTF_MOVE = 0x1;
+        const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        const int MOUSEEVENTF_LEFTUP = 0x4;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x8;
+        const int MOUSEEVENTF_RIGHTUP = 0x10;
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x20;
+        const int MOUSEEVENTF_MIDDLEUP = 0x40;
+        const int MOUSEEVENTF_WHEEL = 0x800;
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+
         private bool _isFirstLoad = true;
 
         public Uri MainUrl { get; set; }
@@ -32,7 +47,7 @@ namespace WebBrowserWPF.Views.UserControls
                 }
             };
 
-            ////WPF
+            //WPF
             //WebBrowser_Main.Navigating += (s, e) =>
             //{
             //    FieldInfo fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
@@ -78,5 +93,26 @@ namespace WebBrowserWPF.Views.UserControls
             }
         }
 
+        private void Button_Up_Click(object sender, RoutedEventArgs e)
+        {
+            var x = System.Windows.Forms.Cursor.Position.X;
+            var y = System.Windows.Forms.Cursor.Position.Y;
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(10, y);
+            //WebBrowser_Main.Focus();
+            WebBrowser_Main.Document.Focus();
+
+            mouse_event(MOUSEEVENTF_WHEEL, 0, 0, 500, 0);
+        }
+
+        private void Button_Down_Click(object sender, RoutedEventArgs e)
+        {
+            var x = System.Windows.Forms.Cursor.Position.X;
+            var y = System.Windows.Forms.Cursor.Position.Y;
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(10, y);
+            //WebBrowser_Main.Focus();
+            WebBrowser_Main.Document.Focus();
+
+            mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -500, 0);
+        }
     }
 }
